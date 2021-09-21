ff1d22269b74950cb8cb59f0383a18cf23aeb608
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-09-21 17:20:58 -0300

    Finaliza requisito 12

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="text" data-testid="search-input" />
+    </div>
+  );
+}
+
+export default SearchBar;
