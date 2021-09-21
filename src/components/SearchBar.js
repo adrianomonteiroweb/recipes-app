865263a8dfaefce97f5c3c745bd92cf458aac016
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-09-21 18:30:57 -0300

    Finaliza requisito 13

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function SearchBar() {
   return (
-    <div>
+    <div className="search-container">
       <input type="text" data-testid="search-input" />
-      <label htmlFor="">
-        <input type="radio" data-testid="ingredient-search-radio" name="radio" id='ingredient' />
-      </label>
+      <div>
 
-      <input type="radio" data-testid="name-search-radio" name="radio" />
-      <input type="radio" data-testid="first-letter-search-radio" name="radio" />
+        <label htmlFor="ingredient">
+          <input
+            type="radio"
+            data-testid="ingredient-search-radio"
+            name="radio"
+            id="ingredient"
+          />
+          Ingrediente
+        </label>
+        <label htmlFor="name">
+          <input type="radio" data-testid="name-search-radio" name="radio" id="name" />
+          Nome
+        </label>
+        <label htmlFor="firstLetter">
+          <input
+            type="radio"
+            data-testid="first-letter-search-radio"
+            name="radio"
+            id="firstLetter"
+          />
+          Primeira letra
+        </label>
+      </div>
+      <button type="button" data-testid="exec-search-btn">Buscar</button>
     </div>
   );
 }
