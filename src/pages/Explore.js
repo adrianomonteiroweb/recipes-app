d97d115a7c167b1180353b4d4617e41e3f0b626d
Gabriel Biasoli <bielbiasoli33@icloud.com>
2021-09-27 16:54:58 -0300

    Finaliza página 'explorar'

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 function Explore() {
   return (
     <>
       <h1>Explore</h1>
+      <div className="container">
+        <Link to="/explorar/comidas" className="row" data-testid="explore-food">
+          Explorar Comidas
+        </Link>
+        <Link to="/explorar/bebidas" className="row" data-testid="explore-drinks">
+          Explorar Bebidas
+        </Link>
+      </div>
       <Footer />
     </>
   );
---
+++ src/pages/ExploreByType.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+function ExploreByType() {
+  return (
+    <>
+      <h1>Explore</h1>
+      <Footer />
+    </>
+  );
+}
+export default ExploreByType;
